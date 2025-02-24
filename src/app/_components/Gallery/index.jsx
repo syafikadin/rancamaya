ad24f0886fa5ce60cdd5181e8b2b16705e88090c
syafikadin <syafik.adin222@gmail.com>
2025-02-25 06:37:38 +0700

    Change palet color

---
+++ src/app/_components/Gallery/index.jsx
@@ -0,0 +1,44 @@
+import Heading from "@/app/_ui/Heading";
+import styles from "./styles.module.css";
+import Image from "next/image";
+// import { getAllRooms } from "@/app/_lib/supabase/rooms";
+// const SUPABASE_ROOMS_URL = process.env.NEXT_PUBLIC_SUPABASE_IMGS_URL;
+async function Gallery() {
+  //   const rooms = await getAllRooms();
+  //   rooms.length = 8;
+  return (
+    <section className={styles.gallerySection}>
+      <div className="container">
+        <Heading className="text-center">Gallery</Heading>
+        <div className={styles.galleryGrid}>
+          {/* {rooms.map((item) => (
+            <div key={item.id} className={styles.thumbnail}>
+              <Image fill src={`${SUPABASE_ROOMS_URL}/${item.thumbnail}`} alt="" />
+            </div>
+          ))} */}
+
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room2.jpg"} alt="" />
+          </div>
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room3.jpg"} alt="" />
+          </div>
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room1.jpg"} alt="" />
+          </div>
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room3.jpg"} alt="" />
+          </div>
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room1.jpg"} alt="" />
+          </div>
+          <div className={styles.thumbnail}>
+            <Image fill src={"/room2.jpg"} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import styles from "./styles.css";
 import About from "./_components/About";
 import Rooms from "./_components/Rooms";
 import HeroSection from "./_components/HeroSection";
+import Gallery from "./_components/Gallery";
+import Blog from "./_components/Blog";
 
 export default async function Home() {
   async function bookingSearchAction(formatedRange) {
@@ -14,6 +16,8 @@ export default async function Home() {
       <HeroSection bookingSearchAction={bookingSearchAction} />
       <About />
       <Rooms />
+      <Gallery />
+      <Blog />
     </>
   );
 }
